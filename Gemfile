6014e20d37a7859f8982674a4177bd7f8b17ece2
yelmasry <yarra.elmasry@gmail.com>
2025-02-04 18:26:30 +0000

    stuck

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 source "https://rubygems.org"
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
+gem "dotenv"
+
 ruby "3.2.1"
 
 gem "sinatra"
--- app.rb
+++ app.rb
@@ -1,9 +1,49 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  @raw_response = HTTP.get("https://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCHANGE_RATE_KEY")}")
+
+  @string_response = @raw_response.to_s
+
+  @parsed_response = JSON.parse(@string_response)
+
+  @currencies = @parsed_response.fetch("currencies")
+
+  erb(:home)
 end
+
+
+get("/:first_symbol") do
+  @the_symbol = params.fetch("first_symbol")
+
+  @raw_response = HTTP.get("https://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCHANGE_RATE_KEY").chomp}")
+
+  @string_response = @raw_response.to_s
+
+  @parsed_response = JSON.parse(@string_response)
+
+  @currencies = @parsed_response.fetch("currencies")
+
+  erb(:step_one)
+end 
+
+
+
+get("/:from_currency/:to_currency") do
+  @from = params.fetch("from_currency")
+  @to = params.fetch("from_currency")
+
+  @url = "https://api.exchangerate.host/convert?from=#{@from}&to=#{@to}=INR&amount=1&access_key=#{ENV.fetch("EXCHANGE_RATE_KEY").chomp}"
+
+  @raw_response = HTTP.get(@url)
+
+  @string_response = @raw_response.to_s
+
+  @parsed_response = JSON.parse(@string_response)
+
+  @amount = @parsed_response.fetch("result")
+
+  erb(:step_two)
+end 
